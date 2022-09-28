528b8ecff62970ae2b8b011c508229ed36c23f77
Sharib Ansari <112088064+sharibllb@users.noreply.github.com>
2022-09-28 21:05:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/contactbike',{useNewUrlParser: true})
+ 
+const port = 80;
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    mobile: Number,
+    email: String,
+    textarea: String
+})
+
+const contact = mongoose.model('contact', contactSchema);
+// console.log (path.join(__dirname, '/static'));
+
+const staticPath = (path.join(__dirname, "/static"))
+// app.set('/static', path.join(__dirname, '/static'));
+// app.set('view engine', 'html')
+
+app.use (express.static(staticPath));
+// app.use('/static', express.static('static'));
+app.use(express.urlencoded())
+
+// app.get("/", (req, res)=>{
+//     res.status(200).send("This is static file")
+// })
+
+// app.get("/contact",(_req, res)=>{
+//     res.status(200).send("contact.html")
+// })
+
+app.post('/contact', (req, res) =>{
+    var myData = new contact(req.body)
+    myData.save().then(()=>{
+        res.send("These item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("The item was not saved to the database")
+    })
+})
 
 
-let images = ['slide2.jpg', 'slide3.jpg', 'slide4.jpeg', 'slide5.jpg', 'slide1.jpg'];
-
-let g = 0;
-
-function slide() {
-    document.getElementById('slides').src = images[g];
-    if (g < images.length - 1) {
-        g++;
-    }
-    else {
-        g = 0;
-    }
-
-}
-setInterval(slide, 4000)
+app.listen(port,()=>{
+    console.log(`The server is running on port ${port}`);
+})
